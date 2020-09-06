ea746740a9611808e775a7a7b92264720f9b909e
Marcos E. Yanes <maeyanes@gmail.com>
2020-09-06 17:53:32 -0500

    fix error merging brew files

--- fish/functions/bbc.fish
+++ fish/functions/bbc.fish
@@ -3,7 +3,9 @@ function bbc
 	set --local BREW ~/.system-config/brew/Brewfile_brew
 	set --local CASK ~/.system-config/brew/Brewfile_cask
 	set --local MAS ~/.system-config/brew/Brewfile_mas
+	set --local BREWFILE ~/.system-config/brew/Brewfile
 
 	cat $TAP $BREW $CASK $MAS > $BREWFILE
-    brew bundle cleanup --file=$BREWFILE --force
+	brew bundle cleanup --file=$BREWFILE --force
+	rm $BREWFILE
 end
