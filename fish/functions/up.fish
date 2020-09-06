247c6d6c3df7d1de575f9cd4515d4c014494ccac
Marcos E. Yanes <maeyanes@gmail.com>
2020-09-06 14:24:13 -0500

    update up function

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,13 +1,29 @@
 function up
+    sudo -v
+
+    echo -e '####################################\n# Pull repos \n####################################'
+    pull_repos
+
     echo -e '####################################\n# Software Update \n####################################'
     sudo softwareupdate --install --all
+
     echo -e '####################################\n# Brew \n####################################'
-    brew update; brew upgrade; brew cask upgrade; brew cleanup; brew doctor;
+    bbc
+    brew update
+    brew upgrade
+    mas upgrade
+    brew cask outdated --greedy ..verbose | ack --invert-match latest | awk '{print $1;}' | xargs brew cask upgrade
+    brew cleanup
+    brew doctor
+
     echo -e '####################################\n# Pip \n####################################'
-    pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests tmuxp virtualenv
+    pip-sync ~/.system-config/pip/requirements.txt
+
     echo -e '####################################\n# Yarn \n####################################'
-    yarn global upgrade --silent
+    yarn global upgrade --silent 2>&1 \ ack --invert-match warning
+
     echo -e '####################################\n# Oh-My-Fish \n####################################'
-    omf install; omf update;
+    omf update;
+
     echo -e '####################################\n# Done \n####################################'
 end
