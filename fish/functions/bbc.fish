90b22d4deb506779a4212399f99778b3751862f7
Marcos E. Yanes <maeyanes@gmail.com>
2020-09-06 14:29:09 -0500

    update bbc to use brew files

--- fish/functions/bbc.fish
+++ fish/functions/bbc.fish
@@ -1,3 +1,9 @@
 function bbc
-    brew bundle cleanup --file=~/.system-config/brew/macOS.Brewfile --force
+	set --local TAP ~/.system-config/brew/Brewfile_tap
+	set --local BREW ~/.system-config/brew/Brewfile_brew
+	set --local CASK ~/.system-config/brew/Brewfile_cask
+	set --local MAS ~/.system-config/brew/Brewfile_mas
+
+	cat $TAP $BREW $CASK $MAS > $BREWFILE
+    brew bundle cleanup --file=$BREWFILE --force
 end
